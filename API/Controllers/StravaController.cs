5abc8a8cd92fa605038e5a58464dc7a6ad7e28fe
Todd Metcalf <toddmetcalf@gmail.com>
2024-08-20 16:38:02 -0400

    Added initial Authorization workflow

--- API/Controllers/StravaController.cs
+++ API/Controllers/StravaController.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Athlete = com.strava.v3.api.Athletes.Athlete;
 using API.Models;
+using com.strava.v3.api.Common;
 
 namespace API
 {
@@ -19,6 +20,84 @@ namespace API
     [ApiController]
     public class StravaController(DataContext context) : ControllerBase
     {
+
+
+        [HttpGet]
+        [Route("GetClientId")]
+        public async Task<ActionResult<int>> GetClientId()
+        {
+            Models.Oauth? oauth = await context.Oauths.FirstOrDefaultAsync();
+            return oauth == null ? NotFound(0) : oauth.ClientID;
+        }
+
+        public class AccessModel
+        {
+            public string code { get; set; }
+            public string scope { get; set; }
+            public string state { get; set; }
+        }
+        [HttpPost]
+        [Route("UpdateAccessToken")]
+        // public async Task<ActionResult<Models.Athlete>> UpdateAccessToken([FromBody] string code, [FromBody] string scope, [FromBody] string state)
+        public async Task<ActionResult<Models.Athlete>> UpdateAccessToken([FromBody] AccessModel accessModel)
+        {
+            Models.User? user = await context.Users.FirstOrDefaultAsync();
+            string code = accessModel.code;
+            string scope = accessModel.scope;
+            string state = accessModel.state;
+            if (user == null)
+            {
+                user = new Models.User
+                {
+                    AccessCode = code,
+                    Name = "",
+                    AccessToken = "",
+                    RefreshToken = "",
+                    ExpiresAtEpoch = 0,
+                    Scope = scope
+                };
+            }
+            user.AccessCode = code;
+            user.Scope = scope;
+
+            List<Models.Oauth> oauths = await context.Oauths.ToListAsync();
+            if (oauths.Count != 1)
+            {
+                throw new Exception("wrong number oAuth rows");
+            }
+            Models.Oauth oauth = oauths[0];
+
+            String url = String.Format("https://www.strava.com/oauth/token?client_id={0}&client_secret={1}&code={2}&grant_type=authorization_code", oauth.ClientID, oauth.ClientSecret, code);
+            String json = await com.strava.v3.api.Http.WebRequest.SendPostAsync(new Uri(url));
+
+            AccessToken accessToken = Unmarshaller<AccessToken>.Unmarshal(json);
+            long.TryParse(accessToken.ExpiresAt, out long tmp);
+            user.ExpiresAtEpoch = tmp;
+            user.AccessToken = accessToken.Token;
+            user.RefreshToken = accessToken.RefreshToken;
+            context.SaveChanges();
+
+            StaticAuthentication auth = new StaticAuthentication(user.AccessToken);
+            // AthleteClient athleteClient = new AthleteClient(auth);
+
+            // Athlete athlete = await athleteClient.GetAthleteAsync();
+            StravaClient client = new StravaClient(auth);
+
+            Athlete athlete = client.Athletes.GetAthlete();
+            Models.Athlete? dbAthlete = context.Athletes.Where(a => a.Id == athlete.Id).FirstOrDefault();
+            if (dbAthlete == null ||
+                DateTime.Parse(dbAthlete.UpdatedAt, CultureInfo.InvariantCulture) < DateTime.Parse(athlete.UpdatedAt, CultureInfo.InvariantCulture))
+            {
+                bool doInsert = dbAthlete == null;
+                dbAthlete = Models.Athlete.ApiAthleteToModel(athlete);
+                if (doInsert)
+                    context.Athletes.Add(dbAthlete);
+                context.SaveChanges();
+            }
+
+
+            return Ok(dbAthlete);
+        }
 
         [HttpGet]
         [Route("SyncAll/{athleteId:long}")]
@@ -107,7 +186,7 @@ namespace API
         {
             Models.Athlete? dbAthlete = await context.Athletes.FirstOrDefaultAsync();
 
-            return dbAthlete == null ? NotFound("Athlete not found. Run authorization") : Ok(dbAthlete);
+            return dbAthlete == null ? NotFound(dbAthlete) : Ok(dbAthlete);
         }
 
         [HttpGet]
--- API/Models/User.cs
+++ API/Models/User.cs
@@ -3,8 +3,8 @@ namespace API.Models;
 public class User
 {
     public int Id { get; set; }
-    public long AthleteId {get;set;}
-    public required string Name { get; set; }
+    public long AthleteId {get;set;} = 0;
+    public required string Name { get; set; } = "";
     public required string AccessToken { get; set; } = "";
     public required string RefreshToken { get; set; } = "";
     public required string AccessCode { get; set; } = "";
